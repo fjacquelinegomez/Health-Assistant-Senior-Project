40256e703dc583da4930891204eb0fd0e1eaa9f4
Fernanda Gomez <77248726+fjacquelinegomez@users.noreply.github.com>
2024-12-11 19:24:40 -0800

    Made changes to the Registration page, created back buttons and linked all pages together (Java).

--- app/src/main/java/com/example/healthassistant/RegisterActivity.java
+++ app/src/main/java/com/example/healthassistant/RegisterActivity.java
@@ -1,26 +1,29 @@
 package com.example.healthassistant;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
+
+
+    private Button next_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login2);
+        setContentView(R.layout.activity_register);
         EdgeToEdge.enable(this);
 
-
+        //logic for once the user presses the back button
         ImageButton backbutton = findViewById(R.id.back_button);
         backbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,11 +32,20 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        //logic for once the user presses the next button
+        next_btn=findViewById(R.id.next_button);
+        next_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(RegisterActivity.this,RegisterActivity2.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
     }
 }

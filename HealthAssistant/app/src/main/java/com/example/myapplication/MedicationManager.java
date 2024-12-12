67346fe5c940bcd8bbc52290a3edc4fb7b09e423
leanhtly <119594124+leanhtly@users.noreply.github.com>
2024-12-11 19:43:21 -0800

    Homescreen

---
+++ HealthAssistant/app/src/main/java/com/example/myapplication/MedicationManager.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MedicationManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_medication_manager);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.androidBackground), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button back = findViewById(R.id.medicationManagerBackButton);
+        View.OnClickListener buttonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.medicationManagerBackButton) {
+                    startActivity(new Intent(MedicationManager.this, Homescreen.class));
+                }
+            }
+        };
+        back.setOnClickListener(buttonClickListener);
+    }
+}

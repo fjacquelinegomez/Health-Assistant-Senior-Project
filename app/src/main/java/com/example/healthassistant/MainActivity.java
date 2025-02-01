c5ef354fb859040770639389dc45bf55b5743820
Fernanda Gomez <77248726+fjacquelinegomez@users.noreply.github.com>
2025-02-01 10:05:04 -0800

    Activated Firebase Real-timedatabase to the project.

--- app/src/main/java/com/example/healthassistant/MainActivity.java
+++ app/src/main/java/com/example/healthassistant/MainActivity.java
@@ -11,6 +11,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -20,8 +23,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("HELLO TEST123!");
+        myRef.setValue("This is me testing, testing, testing again. ");
 
         //logic for once the user presses the login button
         login_btn=findViewById(R.id.login_button);

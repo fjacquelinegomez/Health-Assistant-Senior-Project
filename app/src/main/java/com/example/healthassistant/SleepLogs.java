d9d5963151e4564551345e3f7cfd41b62befbbef
JoshuaA99 <200067538s@gmail.com>
2025-02-14 03:46:41 -0800

    Symsleep

--- app/src/main/java/com/example/healthassistant/SleepLogs.java
+++ app/src/main/java/com/example/healthassistant/SleepLogs.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,21 +21,38 @@ public class SleepLogs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sleep_logs);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button back = findViewById(R.id.sleepLogsBackButton);
-        View.OnClickListener buttonClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (v.getId() == R.id.sleepLogsBackButton) {
-                    startActivity(new Intent(SleepLogs.this, Homescreen.class));
-                }
-            }
-        };
-        back.setOnClickListener(buttonClickListener);
+        //Button back = findViewById(R.id.symptomLogsBackButton);
+        //back.setOnClickListener(v -> startActivity(new Intent(SleepLogs.this, Homescreen.class)));
+
+        TableLayout tableLayout = findViewById(R.id.tableLayout);
+
+        TableRow tableRow = new TableRow(this);
+
+        TextView dateText = new TextView(this);
+        dateText.setText("02/12/2024");
+        dateText.setPadding(20, 8, 8, 8);
+
+        TextView hoursSleptText = new TextView(this);
+        hoursSleptText.setText("7.5 hrs");
+        hoursSleptText.setPadding(20, 8, 8, 8);
+
+        TextView sleepQualityText = new TextView(this);
+        sleepQualityText.setText("9");
+        sleepQualityText.setPadding(20, 8, 8, 8);
+
+        // Add TextViews to the TableRow
+        tableRow.addView(dateText);
+        tableRow.addView(hoursSleptText);
+        tableRow.addView(sleepQualityText);
+
+        // Add TableRow to TableLayout
+        tableLayout.addView(tableRow);
     }
 }

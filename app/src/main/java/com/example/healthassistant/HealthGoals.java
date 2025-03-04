9abe3530329c7d9686cc95e39de58cf1f6cbe51e
Fernanda Gomez <77248726+fjacquelinegomez@users.noreply.github.com>
2025-03-04 14:09:27 -0800

    Further work on food preferences.

--- app/src/main/java/com/example/healthassistant/HealthGoals.java
+++ app/src/main/java/com/example/healthassistant/HealthGoals.java
@@ -11,17 +11,47 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.healthassistant.databinding.ActivityFoodManagerBinding;
+
 public class HealthGoals extends AppCompatActivity {
+
+    ActivityFoodManagerBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_health_goals);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+
+            /**bottom bar navigation functionality**/
+            binding = ActivityFoodManagerBinding.inflate(getLayoutInflater());
+
+            setContentView(binding.getRoot());
+
+            binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        startActivity(new Intent(HealthGoals.this, Homescreen.class));
+                        break;
+                    case R.id.searchMedication:
+                        startActivity(new Intent(HealthGoals.this, Search.class));
+                        break;
+                    case R.id.foodManager:
+                        startActivity(new Intent(HealthGoals.this, FoodManager.class));
+                        break;
+                    case R.id.healthGoals:
+                        startActivity(new Intent(HealthGoals.this, HealthGoals.class));
+                        break;
+                    case R.id.medicationManager:
+                        startActivity(new Intent(HealthGoals.this, MedicationManager.class));
+                        break;
+
+                }
+                return true;
+
         });
+
     }
 }
